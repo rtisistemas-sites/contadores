5abe9c8b00ac353ed5a8018d607c9be9fc66e5ac
rtisistemas <bruno@rtisistemas.com.br>
2019-06-28 10:06:40 -0300

    Merge origin/master
    Conflicts:
    	resources/views/contato.blade.php

--- resources/views/#header.blade.php
+++ resources/views/#header.blade.php
@@ -51,8 +51,8 @@ $caminhoFato = 'https://fatogerador.net/painelUnico/public/';
 
 
 
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <link href="{{ asset('/../rticontadores_git/public/css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('/../rticontadores_git/public/css/style.css') }}" rel="stylesheet">
 
         <!-- incluidos -->
 
@@ -65,14 +65,14 @@ $caminhoFato = 'https://fatogerador.net/painelUnico/public/';
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
-        <?php include 'css/style.php'; ?>
+        <?php include '/../rticontadores_git/public/css/style.php'; ?>
 
-        <link type="text/css" rel="stylesheet" href="{{ asset('css/style-portfolio.css') }}" media="screen">
-        <link type="text/css" rel="stylesheet" href="{{ asset('css/pro-bars.min.css') }}" media="screen">
-        <link type="text/css" rel="stylesheet" href="{{ asset('css/animate.min.css') }}" media="screen">
-        <link type="text/css" rel="stylesheet" href="{{ asset('css/rotator.css') }}" media="screen">
+        <link type="text/css" rel="stylesheet" href="{{ asset('/../rticontadores_git/public/css/style-portfolio.css') }}" media="screen">
+        <link type="text/css" rel="stylesheet" href="{{ asset('/../rticontadores_git/public/css/pro-bars.min.css') }}" media="screen">
+        <link type="text/css" rel="stylesheet" href="{{ asset('/../rticontadores_git/public/css/animate.min.css') }}" media="screen">
+        <link type="text/css" rel="stylesheet" href="{{ asset('/../rticontadores_git/public/css/rotator.css') }}" media="screen">
 
-        <link type="text/css" rel="stylesheet" href="{{ asset('css/bootstrap.css') }}" media="screen">
+        <link type="text/css" rel="stylesheet" href="{{ asset('/../rticontadores_git/public/css/bootstrap.css') }}" media="screen">
         <!-- Google Font Styles -->
         <link href='https://fonts.googleapis.com/css?family=Raleway:400,300' rel='stylesheet' type='text/css'>
         <link href='https://fonts.googleapis.com/css?family=Montserrat+Subrayada:700' rel='stylesheet' type='text/css'>
