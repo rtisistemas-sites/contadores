605660e4fe7a61a4345614ee739d501ac3c91281
rtisistemas <bruno@rtisistemas.com.br>
2019-07-01 17:23:45 -0300

    Alteração 01/07/19 17:23

--- resources/views/#header.blade.php
+++ resources/views/#header.blade.php
@@ -57,7 +57,6 @@ $caminhoFato = 'https://fatogerador.net/painelUnico/public/';
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
-
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/superslides/0.6.2/stylesheets/superslides.min.css">
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
