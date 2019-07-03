632391c812d437419705b0d8a1426dd78efee983
rtisistemas-sites <bruno@rtisistemas.com.br>
2019-07-03 09:30:54 -0300

    Alteração 03/07/19 09:30

--- resources/views/#header.blade.php
+++ resources/views/#header.blade.php
@@ -47,7 +47,7 @@ $caminhoFato = 'https://fatogerador.net/painelUnico/public/';
         <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
         <script  type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-        <!-- END BLOCK TESTE   ----------   -->
+        <!--       END BLOCK TESTE   ----------   -->
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ asset('css/style.css') }}" rel="stylesheet">
